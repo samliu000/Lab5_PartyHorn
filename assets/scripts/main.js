c79c0070fbd6b624c9a833310b5aac628b8f5a6c
samliu000 <44206874+samliu000@users.noreply.github.com>
2021-02-03 12:51:32 -0800

    VolumeBar

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -13,7 +13,7 @@ volumeInput.addEventListener('input', updateVolumeBar);
 function updateVolumeBar(){
     volumeBar.value = volumeInput.value;
     audioPlay.volume = volumeInput.value/100;
-    volumeIcon.src="./assets/media/icons/volume-level-2.svg"
+    volumeIcon.src="./assets/media/icons/volume-level-2.svg";
 }
 
 // update volume box when slider changes
@@ -21,5 +21,5 @@ volumeBar.addEventListener('mouseup', updateVolumeText);
 function updateVolumeText() {
     volumeInput.value = volumeBar.value;
     audioPlay.volume = volumeBar.value/100;
-    volumeIcon.src="./assets/media/icons/volume-level-1.svg"
+    volumeIcon.src="./assets/media/icons/volume-level-1.svg";
 }
