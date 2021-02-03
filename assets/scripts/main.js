dea66e9548f240558d470dab546f9e148fa2c0ee
samliu000 <44206874+samliu000@users.noreply.github.com>
2021-02-03 12:42:47 -0800

    VolumeBar

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,10 +1,19 @@
 // main.js
 
 // TODO
-const volumeInput = document.getElementById('volume-number');
-volumeInput.addEventListener('input', updateVolumeBar);
 
+// find volume elements
+const volumeInput = document.getElementById('volume-number');
+const volumeBar = document.getElementById('volume-slider');
+
+// update volume bar when user inputs in the box
+volumeInput.addEventListener('input', updateVolumeBar);
 function updateVolumeBar(){
-    const volumeBar = document.getElementById('volume-slider');
     volumeBar.value = volumeInput.value;
+}
+
+// update volume box when slider changes
+volumeBar.addEventListener('change', updateVolumeText);
+function updateVolumeText() {
+    volumeInput.value = volumeBar.value;
 }
