22786eee7bf4bda6e4b58e8561f1c14e911b048a
samliu000 <44206874+samliu000@users.noreply.github.com>
2021-02-03 14:04:52 -0800

    VolumeBar

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -7,6 +7,7 @@ const volumeInput = document.getElementById('volume-number');
 const volumeBar = document.getElementById('volume-slider');
 const audioPlay = document.getElementById('horn-sound');
 const volumeIcon = document.getElementById('volume-image');
+const audioSelection = document.getElementById('audio-selection');
 
 // update volume bar when user inputs in the box
 volumeInput.addEventListener('input', updateVolumeBar);
@@ -17,13 +18,14 @@ function updateVolumeBar(){
 }
 
 // update volume box when slider changes
-volumeBar.addEventListener('drag', updateVolumeText);
+volumeBar.addEventListener('drop', updateVolumeText);
 function updateVolumeText() {
     volumeInput.value = volumeBar.value;
     audioPlay.volume = volumeBar.value/100;
     updateVolumeIcon(volumeBar.value);
 }
 
+// function to update the volume icon
 function updateVolumeIcon(vol){
     if(vol >= 67) {
         volumeIcon.src="./assets/media/icons/volume-level-3.svg";
@@ -36,5 +38,24 @@ function updateVolumeIcon(vol){
     }
     else {
         volumeIcon.src="./assets/media/icons/volume-level-0.svg";
+    }
+}
+
+// changes the sounds
+audioSelection.addEventListener('change', changeSound);
+
+function changeSound() {
+    const airHorn = document.getElementById('radio-air-horn');
+    const carHorn = document.getElementById('radio-car-horn');
+    const partyHorn = document.getElementById('radio-party-horn');
+
+    if(carHorn.checked == true) {
+        audioPlay.src="./assets/media/audio/car-horn.mp3";
+    }
+    else if(partyHorn.check == true) {
+        audioPlay.src="./assets/media/audio/party-horn.mp3";
+    }
+    else {
+        audioPlay.src="./assets/media/audio/air-horn.mp3";
     }
 }
